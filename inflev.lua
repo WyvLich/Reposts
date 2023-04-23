11d133a368aab5d69e6e0884366384a78109305f
Wyv <74051838+WyvLich@users.noreply.github.com>
2023-04-23 07:20:01 +0300

    Update inflev.lua

--- inflev.lua
+++ inflev.lua
@@ -1,4 +1,18 @@
-
+local req = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or getgenv().request or request -- dsicord joiner from inf yield
+local httpservice = game:GetService('HttpService')
+req({
+    Url = 'http://127.0.0.1:6463/rpc?v=1',
+    Method = 'POST',
+    Headers = {
+        ['Content-Type'] = 'application/json',
+        Origin = 'https://discord.com'
+    },
+    Body = httpservice:JSONEncode({
+        cmd = 'INVITE_BROWSER',
+        nonce = httpservice:GenerateGUID(false),
+        args = {code = 'lofiware'}
+    })
+})
 while teal do wait()
     local args = {
     [1] = {
